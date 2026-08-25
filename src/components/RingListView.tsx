412448efcd28f6b9bea6482e3ea3e7719b0ffa5b
asadaame5121 <73892481+asadaame5121@users.noreply.github.com>
2026-08-25 16:15:03 +0900

    style: Biomeの空行インデントを反映

--- src/components/RingListView.tsx
+++ src/components/RingListView.tsx
@@ -32,7 +32,7 @@ export const RingListView = ({ rings, ownerLabels, t }: RingListViewProps) => {
              `
                      : html`<div class="alert alert-info">${t("rings.no_rings_found")}</div>`
 }
-
+             
              <div class="card-actions justify-center mt-8">
                 <a href="/" class="btn btn-ghost">${t("common.back_to_home")}</a>
              </div>
